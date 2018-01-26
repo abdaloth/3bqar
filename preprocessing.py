8f34d7225f6687be153e67122eae623338d225b7
Master <s201245200@kfupm.edu.sa>
2018-01-27 01:03:56 +0300

    modified text cleaner
    added limit to maximum line width and maximum char count

--- preprocessing.py
+++ preprocessing.py
@@ -5,10 +5,14 @@ from gensim.models import Word2Vec
 import itertools
 import re
 
+MAX_WORDS_PER_LINE = 15
+MAX_CHAR_PER_WORD = 15
 # remove harakat
 harakat_re = r'(ٍ|َ|ُ|ِ|ّ|ْ|ً)'
-# remove every non arabic charachter that isnt a space or a newline
-nonarabic_nonspace_re = r'[^\u0621-\u064A \n]'
+# remove every non arabic charachter that isnt a whitespace
+nonarabic_nonspace_re = r'[^\u0621-\u064A\s]'
+# normalize whitespace
+whitespace_re = r'[^\S\n]'
 # remove word extensions e.g. أهـــــــــلا
 extension_re = r'ـ'
 # normalize alef
@@ -28,23 +32,25 @@ def normalize(poem):
                              poem_normalized, flags=re.UNICODE)
     poem_normalized = re.sub(nonarabic_nonspace_re, ' ',
                              poem_normalized, flags=re.UNICODE)
-    # compact repetitive spaces
-    poem_normalized = re.sub(r'( ){2,}', r' ',
+    poem_normalized = re.sub(whitespace_re, ' ',
+                             poem_normalized, flags=re.UNICODE)
+    # compact repetitive chars
+    poem_normalized = re.sub(r'(.)\1{3,}', r'\1',
                              poem_normalized, flags=re.UNICODE)
     poem_normalized = re.sub(alef_re, 'ا',
                              poem_normalized, flags=re.UNICODE)
-    poem_normalized = re.sub(waw_hamzah_re, 'و',
-                             poem_normalized, flags=re.UNICODE)
-    poem_normalized = re.sub(yaa_hamzah_re, 'ي',
-                             poem_normalized, flags=re.UNICODE)
+    # poem_normalized = re.sub(waw_hamzah_re, 'و',
+    #                          poem_normalized, flags=re.UNICODE)
+    # poem_normalized = re.sub(yaa_hamzah_re, 'ي',
+    #                          poem_normalized, flags=re.UNICODE)
     poem_normalized = re.sub(taa_marbootah_re, 'ه',
                              poem_normalized, flags=re.UNICODE)
     return poem_normalized
 
 
-def create_w2v_model(verses):
-    verses = [s.split() for v in verses for s in v]
-    model = Word2Vec(verses,size=300, window=3,sg=1,workers=4,iter=25)
+def create_w2v_model(poems):
+    verses = [verse for poem in poems for verse in poem]
+    model = Word2Vec(verses, size=300, window=3, sg=1, workers=4, iter=25)
     model.save('word2vec')
 
 # %%
@@ -54,17 +60,13 @@ if __name__ == '__main__':
     data = data.drop_duplicates()
     len(data)
     data = data[data.poem_text.notnull()]
-    poems = data.poem_text.apply(lambda p: normalize(p).strip())
-    poems = poems.drop_duplicates()
-    len(poems)
-    # poems = poems.apply(lambda x: x.split('\n'))
-    poems = poems.tolist()
-    verses = [p.split('\n') for p in poems]
-    # create_w2v_model(verses)
-    poems = list(itertools.chain.from_iterable(verses))
-    strip = str.rstrip
-    raw_text = '\n'.join([line for line in poems if(1 < len(strip(line, '\n')) <=100)]).rstrip('\n')
-    # raw_text = re.sub(r'\s{2,}', '\n', raw_text, flags=re.UNICODE)
-    raw_text[:100]
+    poems = data.poem_text.apply(lambda p: normalize(p))
+    poems = poems.apply(lambda p: p.split('\n'))
+    poems = poems.apply(lambda p: [v.split() for v in p])
+    # create_w2v_model(poems)
+    poems = [p for p in poems if len(p) <= MAX_WORDS_PER_LINE]
+    poems = [[word for word in poem  if len(word)< MAX_CHAR_PER_WORD] for poem in poems]
+    poems = [' '.join(p) for p in poems]
+    raw_text = '\n'.join(poems)
     with open('normalized_poem_text.txt', 'w', encoding='utf8') as f:
         f.write(raw_text)
