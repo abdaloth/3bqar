349901ac2cd3fb6b8d7fa875ff9225af622942a4
Master <s201245200@kfupm.edu.sa>
2018-02-01 22:55:29 +0300

    first working model

--- word_char_model.py
+++ word_char_model.py
@@ -1,18 +1,38 @@
 import pandas as pd
 import numpy as np
-from preprocessing import NA, MAX_CHAR_PER_WORD
+from preprocessing import MAX_CHAR_PER_WORD
 from gensim.models import Word2Vec
 
 from keras import backend as K
-from keras.utils import to_categorical
+from keras.utils import to_categorical, Sequence
 from keras.callbacks import ModelCheckpoint, TensorBoard
 
 from keras.models import Model
 from keras.layers import Flatten, Dense, Input
-from keras.layers import CuDNNLSTM, Bidirectional, Embedding, concatenate
+from keras.layers import CuDNNLSTM, Bidirectional, Embedding, GlobalMaxPooling1D, concatenate
+
+
+class PoemSequence(Sequence):
+
+    def __init__(self, inputs, output, batch_size, num_classes):
+        self.x_c, self.x_w = inputs
+        self.y = output
+        self.batch_size = batch_size
+        self.num_classes = num_classes
+
+    def __len__(self):
+        return round(len(self.y) / self.batch_size)
+
+    def __getitem__(self, idx):
+        batch_x = [self.x_c[(idx * self.batch_size):((idx + 1) * self.batch_size)
+                           ], self.x_w[(idx * self.batch_size):((idx + 1) * self.batch_size)]]
+        batch_y = self.y[(idx * self.batch_size):((idx + 1) * self.batch_size)]
+
+        return batch_x, to_categorical(batch_y, num_classes=self.num_classes)
+
 
 def pad_w(w):
-    return w + [c2i[NA]] * (MAX_CHAR_PER_WORD - len(w))
+    return w + [c2i[UNKNOWN]] * (MAX_CHAR_PER_WORD - len(w))
 
 
 def get_embedding_matrix():
@@ -21,12 +41,15 @@ def get_embedding_matrix():
     embedding_matrix[:-1] = word_vec.syn0
     return embedding_matrix
 
+
+UNKNOWN = '_'
 # load word vector and create index2value and value2index dicts
 word_vec = Word2Vec.load('w2v/word2vec').wv
 i2w = dict(enumerate(word_vec.index2word))
 w2i = dict([(v, k) for k, v in i2w.items()])
-# add NA to the dictionary for unknown chars
-i2c = dict(enumerate(sorted(set(' '.join(word_vec.index2word) + NA))))
+CLASS_NUM = len(i2w) + 1
+# add UNKNOWN to the dictionary for unknown chars
+i2c = dict(enumerate(sorted(set(' '.join(word_vec.index2word) + UNKNOWN))))
 c2i = dict([(v, k) for k, v in i2c.items()])
 
 # load verses and flatten into one continuous array
@@ -35,9 +58,9 @@ X_w_flat = X_w.flatten()
 
 # load word-chars and flatten into one continuous array
 X_ch = pd.read_csv('data/verses.csv', encoding='utf-8')
-X_ch = X_ch.applymap(lambda v: pad_w([c2i.get(w, c2i[NA]) for w in v]))
+X_ch = X_ch.applymap(lambda v: pad_w([c2i.get(w, c2i[UNKNOWN]) for w in v]))
 X_ch = np.array(X_ch.values.tolist())
-X_ch_flat = X_ch.reshape((X_ch.shape[0]*X_ch.shape[1], X_ch.shape[2]))
+X_ch_flat = X_ch.reshape((X_ch.shape[0] * X_ch.shape[1], X_ch.shape[2]))
 
 seq_length = 8  # play with this number
 step_size = 3  # and this number
@@ -51,21 +74,35 @@ for i in range(0, X_w_flat.shape[0] - seq_length, step_size):
     words_out.append(X_w_flat[i + seq_length])
 
 words_in = np.stack(words_in)
-chars_in =  np.stack(chars_in)
-words_out = to_categorical(words_out, num_classes=len(i2w))
-chars_in.shape
-words_out.shape
-X[1]
-embed = get_embedding_matrix()
+chars_in = np.stack(chars_in)
+
+
+#%%
 K.clear_session()
-char_input = Input(shape=(15, 15,))
-word_input = Input(shape=(15,))
-word_embed = Embedding(output_dim=300, input_dim=embed.shape[0],
+
+char_input = Input(shape=(seq_length, MAX_CHAR_PER_WORD,), name='chars_in')
+char_embed = Bidirectional(CuDNNLSTM(256, return_sequences=True), merge_mode='sum')(char_input)
+
+word_input = Input(shape=(seq_length,), name='words_in')
+embed = get_embedding_matrix()
+word_embed = Embedding(output_dim=256, input_dim=embed.shape[0],
                        weights=[embed], trainable=False)(word_input)
-char_embed = Bidirectional(CuDNNLSTM(300, return_sequences=True), merge_mode='sum')(char_input)
-concat = concatenate([char_embed, word_embed])
-next_word = Dense(y.shape[1], activation='softmax')(concat)
+concat = concatenate([char_embed, word_embed], axis=-1)
+maxpool = GlobalMaxPooling1D()(concat)
+dense = Dense(256, activation='relu')(maxpool)
+next_word = Dense(CLASS_NUM, activation='softmax')(dense)
 model = Model(inputs=[char_input, word_input], outputs=next_word)
-model.compile(optimizer='adam', loss='binary_crossentropy')
+model.compile(optimizer='adam', loss='categorical_crossentropy')
 model.summary()
-# np.zeros((1,15))
+#%%
+tb = TensorBoard(log_dir='logs/word_char0'.format(model.name), histogram_freq=0,
+                     write_graph=True, write_images=False)
+mc = ModelCheckpoint('weights/word_char0-epoch-{epoch:02d}-loss-{loss:.4f}.hdf5',
+                             save_best_only=True, save_weights_only=True, mode='min',
+                             monitor='loss', verbose=1)
+
+generator = PoemSequence(inputs=[chars_in, words_in], output=words_out,
+                         batch_size=512, num_classes=CLASS_NUM)
+model.fit_generator(generator=generator, callbacks=[tb, mc], epochs=1)
+#%%
+##GENERATE TEXT
