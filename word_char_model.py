46b3a3060b8e9c2a66b3689549bada856c7a882b
Master <s201245200@kfupm.edu.sa>
2018-02-04 20:13:20 +0300

    added temperature sampling

--- word_char_model.py
+++ word_char_model.py
@@ -38,7 +38,7 @@ with open('data/raw_text.txt', encoding='utf-8') as f:
 
 UNKNOWN = '_' # token for out-of-vocab
 N_VOCAB = 20000
-G = 0.2 # merging value of character and word embeddings
+G = 0.2 # gating value
 seq_length = 1  # play with this number
 
 # tokenize words into indices
@@ -55,7 +55,7 @@ tokenized_chars = char_tokenizer.texts_to_sequences(word_list)
 tokenized_chars = [c for c,w in zip(tokenized_chars, tokenized_words) if len(w)>0]
 tokenized_words = [w for w in tokenized_words if len(w)>0]
 
-#prepare inputs and outputs for model
+# prepare inputs and outputs for model
 X_w = np.array(list(chain.from_iterable(tokenized_words)))
 y = X_w[1:]
 X_w = np.reshape(X_w[:-1], (X_w.shape[0] - 1, seq_length))
@@ -93,32 +93,47 @@ mc = ModelCheckpoint('weights/word_char-epoch-{epoch:02d}-loss-{loss:.4f}.hdf5',
 
 generator = PoemSequence(inputs=[X_ch, X_w], output=y,
                          batch_size=1024, num_classes=N_VOCAB)
-model.fit_generator(generator=generator, initial_epoch=5, epochs=10, callbacks=[tb, mc])
+model.fit_generator(generator=generator, epochs=100, callbacks=[tb, mc])
 #%%
 w2i = dict(word_tokenizer.word_index)
 i2w = {v:k for k,v in w2i.items()}
 c2i = dict(char_tokenizer.word_index)
 i2c = {v:k for k,v in c2i.items()}
-#%%
-from random import randint
-# GENERATE TEXT
-idx = randint(0, len(X_w))
-w_in = X_w[idx:idx + 1]
-c_in = X_ch[idx:idx + 1]
-sequence = c_in, w_in
-gen_text = ''
-# generate words
-#NOTE consider adding temperature
-for i in range(30):
-    x_c, x_w= sequence
-    prediction = model.predict([x_c, x_w], verbose=0)
-    # returns the index of the predicted word
-    index = np.argmax(prediction[0])
-    gen_text += ' ' + i2w.get(index, '_')
-    x_w[0][0] = index
-    char_idx = [c2i[c] for c in i2w[index]]
-    x_c[0] = pad_sequences([char_idx], maxlen=MAX_CHAR_PER_WORD)
-    sequence = x_c, x_w
 
-print(gen_text)
-print('Done.')
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    # from keras doc
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+#%%
+# GENERATE TEXT
+from random import randint
+for tmp in [.3, .5, 1., 1.2, 1.7]:
+    print(f'temperature = {tmp}')
+    idx = randint(0, len(X_w))
+    w_in = X_w[idx:idx + 1]
+    c_in = X_ch[idx:idx + 1]
+    sequence = c_in, w_in
+    gen_text = ''
+    # generate words
+    #NOTE consider adding temperature
+    for i in range(80):
+        if(i%8 == 0):
+            gen_text += '\n'
+        x_c, x_w= sequence
+        prediction = model.predict([x_c, x_w], verbose=0)[0]
+        # returns the index of the predicted word
+        index = sample(prediction, tmp)
+        gen_text += ' ' + i2w.get(index, '_')
+        x_w[0][0] = index
+        char_idx = [c2i[c] for c in i2w[index]]
+        x_c[0] = pad_sequences([char_idx], maxlen=MAX_CHAR_PER_WORD)
+        sequence = x_c, x_w
+
+    print(gen_text)
+    print('-' * 42)
