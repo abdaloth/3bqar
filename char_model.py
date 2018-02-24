f893ffb17ce9dbd5ea149bc0aa6293e8cb30196c
Master <s201245200@kfupm.edu.sa>
2018-02-24 11:40:19 +0300

    char model v2

--- char_model.py
+++ char_model.py
@@ -1,14 +1,16 @@
 import numpy as np
-from utils import word_list, verses, to_categorical, sample
+from utils import to_categorical, sample
 
 from keras import backend as K
+from keras.optimizers import Adamax
 from keras.callbacks import ModelCheckpoint, TensorBoard
-
 from keras.models import Model
 from keras.layers import Dense, Input, Dropout
 from keras.layers import CuDNNLSTM, Bidirectional, Embedding
 
-
+with open('data/raw_text.txt', encoding='utf-8') as f:
+    verses = [v.strip() for v in f.readlines()]
+    word_list = ' '.join(verses).split()
 # tokenize words into char index list
 raw_text = '\n'.join(verses)
 chars = sorted(set(raw_text))
@@ -16,14 +18,10 @@ c2i = {c: i for i, c in enumerate(chars)}
 i2c = {i: c for i, c in enumerate(chars)}
 
 tokenized_text = np.array([c2i[c] for c in raw_text])
-tokenized_words = [[c2i[c] for c in w] for w in word_list]
-
-word_lengths = [len(w) for w in word_list]
-verse_lengths = [len(v.split()) for v in verses]
 
 # length of input sequences (experiment with this number)
-seq_length = int(np.percentile(verse_lengths, 90) * np.mean(word_lengths))
-stride_offset = 0 # and this number
+seq_length = 100
+stride_offset = 0  # and this number
 
 # prepare inputs and outputs for model
 idx = np.arange(seq_length, len(raw_text), seq_length + stride_offset)
@@ -39,27 +37,38 @@ chars_in = np.stack(chars_in)
 X = np.reshape(chars_in, (chars_in.shape[0], seq_length, 1))
 y = to_categorical(char_out, num_classes=len(chars))
 #%%
+
+
+def build_char_model(weights_path=''):
+    inps = Input(shape=(X.shape[1], X.shape[2]))
+    x = CuDNNLSTM(512, return_sequences=True)(inps)
+    x = Dropout(0.5)(x)
+    x = CuDNNLSTM(512, return_sequences=True)(inps)
+    x = Dropout(0.5)(x)
+    x = CuDNNLSTM(512)(x)
+    x = Dropout(0.5)(x)
+    x = Dense(y.shape[1], activation='softmax')(x)
+    model = Model(inps, x)
+    if(weights_path):
+        model.load_weights(weights_path)
+    model.compile(optimizer=Adamax(decay=1e-8),
+                  loss='categorical_crossentropy')
+    return model
+
+
 K.clear_session()
-# build model
-inps = Input(shape=(X.shape[1], X.shape[2]))
-x = Bidirectional(CuDNNLSTM(256, return_sequences=True))(inps)
-x = Dropout(0.3)(x)
-x = Bidirectional(CuDNNLSTM(256))(x)
-x = Dense(y.shape[1], activation='softmax')(x)
-model = Model(inps, x)
-model.compile(optimizer='adam', loss='categorical_crossentropy')
+model = build_char_model()
 model.summary()
 #%%
-tb = TensorBoard(log_dir='logs/char_model', histogram_freq=0,
+tb = TensorBoard(log_dir='logs/char_model_v2', histogram_freq=0,
                  write_graph=True, write_images=False)
-mc = ModelCheckpoint('weights/char-epoch-{epoch:02d}-loss-{loss:.4f}.hdf5',
+mc = ModelCheckpoint('weights/char_v2-epoch-{epoch:02d}-loss-{loss:.4f}.hdf5',
                      save_best_only=True, mode='min',
                      monitor='loss', verbose=1)
-
-model.fit(X, y, epochs=600, batch_size=1024, callbacks=[tb, mc])
+model.fit(X, y, epochs=1000, batch_size=1024, callbacks=[tb, mc])
 #%%
 # GENERATE TEXT
-for tmp in [.1, .3, .4, .5, .6, .7, .8, .9, 1., 1.2]:
+for tmp in [float(i / 10) for i in range(1, 20)]:
     print(f'temperature = {tmp}')
     idx = np.random.randint(0, X.shape[0] - seq_length)
     sequence = chars_in[idx]
